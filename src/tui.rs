e3ee7acb445439406696e5c26377a2b5f4c04e7e
CodeyBoi <hannes.ryberg00@gmail.com>
2023-09-09 14:02:31 +0200

    Simple TUI
    Added a simple TUI that can run the profiler

--- src/tui.rs
+++ src/tui.rs
@@ -1,71 +1,121 @@
-use crossterm::{
-    cursor,
-    event::{self, DisableMouseCapture, EnableMouseCapture},
-    execute,
-    terminal::{
-        self, disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
-    },
-};
-use ratatui::{
-    prelude::CrosstermBackend,
-    style::{Color, Modifier, Style},
-    widgets::{Block, Borders, List, ListItem, ListState},
-    Terminal,
-};
+use std::io::{self, Write};
 
-use std::{io, thread, time::Duration};
+use crate::{profiler, Bridge};
 
-pub(crate) fn main() -> io::Result<()> {
-    enable_raw_mode()?;
-
-    let mut stdout = io::stdout();
-    execute!(
-        stdout,
-        EnterAlternateScreen,
-        EnableMouseCapture,
-        cursor::Hide,
-        terminal::Clear(terminal::ClearType::All)
-    )?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-
-    terminal.draw(|f| {
-        let mut size = f.size();
-        let block = Block::default().title("KyberKracker").borders(Borders::ALL);
-        f.render_widget(block, size);
-        let items = ["Profile", "Attack", "Exit"];
-        let (list, mut state) = menu(&items);
-        size.height = size.height - 2;
-        size.width = size.width - 2;
-        size.x = 1;
-        size.y = 1;
-        f.render_stateful_widget(list, size, &mut state);
-    })?;
-
-    thread::spawn(|| loop {
-        event::read().unwrap();
-    });
-
-    thread::sleep(Duration::from_millis(5000));
-
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture,
-        cursor::Show
-    )?;
-
-    Ok(())
+fn read_line() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input
 }
 
-fn menu<'a>(items: &'a [&'a str]) -> (List<'a>, ListState) {
-    let items: Vec<_> = items.iter().map(|s| ListItem::new(s.to_string())).collect();
-    let list = List::new(items)
-        .block(Block::default().title("Commands").borders(Borders::ALL))
-        .style(Style::default().fg(Color::White))
-        .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
-        .highlight_symbol(">>");
-    let state = ListState::default();
-    (list, state)
+pub(crate) fn select_command() {
+    let mut stdout = io::stdout();
+
+    println!("Welcome to KyberKracker!\n");
+    println!("This is a tool for profiling and attacking DRAM with RowHammer, with the goal of cracking the Kyber cipher.\n");
+
+    println!("Available commands:");
+    println!("\t1. Profile");
+    println!("\t2. Attack");
+    println!("\t3. Exit\n");
+
+    print!("Select command (1-3): ");
+
+    stdout.flush().unwrap();
+
+    loop {
+        let input = read_line();
+        match input.trim() {
+            "1" => {
+                run_profiler();
+                break;
+            }
+            "2" => {
+                println!("Attacking... >:)");
+                break;
+            }
+            "3" => {
+                println!("Bye!");
+                break;
+            }
+            _ => {
+                print!("Please enter a valid command (1-3): ");
+                stdout.flush().unwrap();
+            }
+        }
+    }
+}
+
+fn run_profiler() {
+    let mut stdout = io::stdout();
+
+    println!("\t1. Run with default settings (-p 0.5 -c 4 -d 2 -b haswell)");
+    println!("\t2. Run with custom settings\n");
+
+    print!("Select command (1-2): ");
+    stdout.flush().unwrap();
+
+    loop {
+        let input = read_line();
+        match input.trim() {
+            "1" => {
+                profiler::rowhammer::main(0.5, 4, 2, Bridge::Haswell);
+                break;
+            }
+            "2" => {
+                let fraction_of_phys_memory: f64 = loop {
+                    print!("Fraction of physical memory to profile (0.0-1.0): ");
+                    stdout.flush().unwrap();
+                    let input = read_line();
+                    if let Ok(f) = input.trim().parse() {
+                        if f >= 0.0 && f <= 1.0 {
+                            break f;
+                        } else {
+                            eprintln!("Value must be in the range 0.0-1.0");
+                        }
+                    } else {
+                        eprintln!("Input must be a valid float number");
+                    }
+                };
+                let cores: u8 = loop {
+                    print!("Number of cores on target machine: ");
+                    stdout.flush().unwrap();
+                    let input = read_line();
+                    if let Ok(c) = input.trim().parse() {
+                        break c;
+                    } else {
+                        eprintln!("Input must be an integer");
+                    }
+                };
+                let dimms: u8 = loop {
+                    print!("Number of RAM sticks on target machine: ");
+                    stdout.flush().unwrap();
+                    let input = read_line();
+                    if let Ok(d) = input.trim().parse() {
+                        break d;
+                    } else {
+                        eprintln!("Input must be an integer");
+                    }
+                };
+                let bridge = loop {
+                    print!("Northbridge type (haswell/sandy): ");
+                    stdout.flush().unwrap();
+                    let input = read_line();
+                    match input.trim() {
+                        "haswell" => break Bridge::Haswell,
+                        "sandy" => break Bridge::Sandy,
+                        _ => eprintln!("Input must be either 'haswell' or 'sandy'"),
+                    }
+                };
+                profiler::rowhammer::main(fraction_of_phys_memory, cores, dimms, bridge);
+                break;
+            }
+            _ => {
+                print!("Please enter a valid command (1-2): ");
+                stdout.flush().unwrap();
+            }
+        }
+    }
 }
