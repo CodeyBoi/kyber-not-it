aef1dce74fd92ff677677025b48efd249b09b4dd
CodeyBoi <hannes.ryberg00@gmail.com>
2023-09-15 10:14:07 +0200

    Refactored rowhammer profiler
    Refactored rowhammer profiler to make it easier to check for 1 to 0 flips in the future

--- src/profiler/rowhammer.rs
+++ src/profiler/rowhammer.rs
@@ -9,12 +9,19 @@ use crate::{
     Bridge,
 };
 
+use super::utils::Row;
+
 const NO_OF_READS: u64 = 27 * 100 * 1000 * 4 / 4;
 
-// const OFF_ON: u16 = 0x5555;
-// const ON_OFF: u16 = 0xaaaa;
-// const STRIPE: u16 = 0x00FF;
-// const FRODO_HAMMER: u16 = 0x0100;
+#[allow(dead_code)]
+const OFF_ON: u16 = 0x5555;
+#[allow(dead_code)]
+const ON_OFF: u16 = 0xaaaa;
+#[allow(dead_code)]
+const STRIPE: u16 = 0x00FF;
+#[allow(dead_code)]
+const FRODO_HAMMER: u16 = 0x0100;
+#[allow(dead_code)]
 const BLAST: u16 = u16::MAX;
 
 const PATTERN: u16 = BLAST;
@@ -45,15 +52,15 @@ fn init_row(row: &[Page], pattern: u16) {
 /// Finds flipped (non-zero) bits in `row`.
 ///
 /// # Returns
-fn find_flips(page: &Page) -> [u64; Consts::MAX_BITS] {
+fn find_flips(page: &Page, init_pattern: u16) -> [u64; Consts::MAX_BITS] {
     let mut flips = [0; Consts::MAX_BITS];
     let base_ptr = page.virt_addr as *const u16;
     for i in 0..Consts::PAGE_SIZE / 2 {
         unsafe {
             let ptr = base_ptr.add(i);
             _mm_clflush(ptr as *const u8);
-            for bit in 0..size_of_val(&INIT_PATTERN) * 8 {
-                flips[bit] += (((*ptr >> bit) & 1) ^ ((INIT_PATTERN >> bit) & 1)) as u64;
+            for bit in 0..size_of_val(&init_pattern) * 8 {
+                flips[bit] += (((*ptr >> bit) & 1) ^ ((init_pattern >> bit) & 1)) as u64;
             }
         }
     }
@@ -110,27 +117,13 @@ fn hammer_all_reachable_pages(
             }
         }
 
-        // Initialize rows (above and below get aggressor pattern, i.e. 0b0101, and target row gets zeroed)
-        init_row(&above_row[..], PATTERN);
-        init_row(&target_row[..], INIT_PATTERN);
-        init_row(&below_row[..], PATTERN);
-
         // Collect a list of addresses that are in the same bank
-        let mut banks = Vec::new();
-        let mut virt_addrs = Vec::new();
-        for above_page in above_row {
-            for below_page in below_row {
-                let above_bank = above_page.bank_index(bridge, dimms);
-                let below_bank = below_page.bank_index(bridge, dimms);
-                if above_bank == below_bank && !banks.contains(&above_bank) {
-                    banks.push(above_bank);
-                    virt_addrs.push((
-                        above_page.virt_addr as *const u8,
-                        below_page.virt_addr as *const u8,
-                    ));
-                }
-            }
-        }
+        let virt_addrs = get_hammer_targets(above_row, below_row, bridge, dimms);
+
+        // Initialize rows (above and below get aggressor pattern, i.e. 0b0101, and target row gets zeroed)
+        init_row(&above_row[..], BLAST);
+        init_row(&target_row[..], 0x0);
+        init_row(&below_row[..], BLAST);
 
         let before = Instant::now();
 
@@ -138,6 +131,8 @@ fn hammer_all_reachable_pages(
         for (above_va, below_va) in virt_addrs {
             rowhammer(above_va, below_va);
         }
+
+        let target_row = collect_data(target_row, above_row, below_row, 0x0);
 
         rows_tested += 1;
         println!(
@@ -147,29 +142,13 @@ fn hammer_all_reachable_pages(
         );
 
         // Count the number of flipped bits in the target row after each test
-        let mut target_row = target_row.clone();
-        for ((above_page, target_page), below_page) in
-            above_row.into_iter().zip(&mut target_row).zip(below_row)
-        {
-            let flips = find_flips(target_page);
-            match target_page.data {
-                Some(ref mut data) => {
-                    for (old_flip, new_flip) in data.flips.iter_mut().zip(flips) {
-                        *old_flip += new_flip;
-                    }
-                }
-                None => {
-                    target_page.data = Some(PageData::new(&above_page, &below_page, flips));
-                }
-            }
-            total_flips += flips.iter().sum::<u64>();
-        }
 
         println!("\tTarget page\tAbove PFN\tBelow PFN\tFlips\tFlipped bits");
         for page in &target_row {
             let flips = page.data.as_ref().unwrap().flips;
             let flip_sum = flips.iter().sum::<u64>();
             if flip_sum > 0 {
+                total_flips += flip_sum;
                 let data = page.data.as_ref().unwrap();
                 println!(
                     ">\t{:#x}\t{:#x}\t{:#x}\t{}\t{:?}",
@@ -190,6 +169,50 @@ fn hammer_all_reachable_pages(
     }
     println!("Done!");
     Ok(())
+}
+
+fn collect_data(row: &Row, above_row: &Row, below_row: &Row, init_pattern: u16) -> Row {
+    let mut row = row.clone();
+    for ((above_page, target_page), below_page) in
+        above_row.into_iter().zip(&mut row).zip(below_row)
+    {
+        let flips = find_flips(target_page, init_pattern);
+        match target_page.data {
+            Some(ref mut data) => {
+                for (old_flip, new_flip) in data.flips.iter_mut().zip(flips) {
+                    *old_flip += new_flip;
+                }
+            }
+            None => {
+                target_page.data = Some(PageData::new(&above_page, &below_page, flips));
+            }
+        }
+    }
+    row
+}
+
+fn get_hammer_targets(
+    above_row: &Row,
+    below_row: &Row,
+    bridge: Bridge,
+    dimms: u8,
+) -> Vec<(*const u8, *const u8)> {
+    let mut banks = Vec::new();
+    let mut virt_addrs = Vec::new();
+    for above_page in above_row {
+        for below_page in below_row {
+            let above_bank = above_page.bank_index(bridge, dimms);
+            let below_bank = below_page.bank_index(bridge, dimms);
+            if above_bank == below_bank && !banks.contains(&above_bank) {
+                banks.push(above_bank);
+                virt_addrs.push((
+                    above_page.virt_addr as *const u8,
+                    below_page.virt_addr as *const u8,
+                ));
+            }
+        }
+    }
+    virt_addrs
 }
 
 pub(crate) fn main(fraction_of_phys_memory: f64, cores: u8, dimms: u8, bridge: Bridge) {
