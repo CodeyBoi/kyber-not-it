2fc0bbb18d98b86b9a073952b192ae3184c88505
PalmerVincent <vincent.palmer@hotmail.com>
2023-11-09 13:13:48 +0100

    Added working degradation script
    Added working degradation script to potentially be used.
    Might need some reworking to be cleaner but it does its purpose.

--- src/degradation/run_degradation.sh
+++ src/degradation/run_degradation.sh
@@ -7,3 +7,29 @@ if [ ! -f degrade ]; then
     gcc degrade.c -L../libs -lmastik -ldwarf -lelf -lbfd -O3 -o degrade
 fi
 
+# Declare an array of taskset masks
+declare -a cpu_masks=("0x2" "0x20" "0x4" "0x1")
+
+run_degradation(){
+    for mask in "${cpu_masks[@]}"; do
+        # Run the degradation binary with the taskset mask
+        taskset $mask ./degrade &
+    done
+}
+
+kill_degradation()  {
+    # Kill all the degradation processes
+    pkill -f "degrade"
+}
+
+# Run degradations
+run_degradation
+
+# Run the FrodoKEM script
+/home/development/Frodo/PQCrypto-LWEKE/frodo640/test_KEM &
+
+# Wait for all the the FrodoKEM process to finish
+wait
+
+# Kill the degradation processes
+kill_degradation
